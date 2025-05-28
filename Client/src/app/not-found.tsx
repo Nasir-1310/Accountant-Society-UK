a5939ebdff87799a01db911e6e32302b38718565
Md. Nasir Uddin <bsse1310@iit.du.ac.bd>
2025-05-28 13:10:12 +0600

    not found page updaged

--- Client/src/app/not-found.tsx
+++ Client/src/app/not-found.tsx
@@ -1,4 +1,5 @@
 // app/not-found.tsx
+import Link from "next/link";
 export default function NotFound() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100 px-4">
@@ -7,14 +8,14 @@ export default function NotFound() {
           🚧 This page is under construction
         </h1>
         <p className="text-lg text-gray-600 mb-6">
-          We're working hard to bring this page to life. Please check back later.
+          We are working hard to bring this page to life. Please check back later.
         </p>
-        <a
+        <Link
           href="/"
           className="inline-block px-6 py-3 bg-blue-600 text-white rounded-xl text-lg hover:bg-blue-900 transition-colors duration-300"
         >
           Go back home
-        </a>
+        </Link>
       </div>
     </div>
   );
