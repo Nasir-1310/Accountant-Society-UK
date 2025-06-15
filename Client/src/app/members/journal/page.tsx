9e169ed1bb1f3c41b0ab4f93e0fdb0237acd4275
Nasir-1310 <bsse1310@iit.du.ac.bd>
2025-06-15 15:40:19 +0600

    home page journal, rules and guidance link added

--- Client/src/app/members/journal/page.tsx
+++ Client/src/app/members/journal/page.tsx
@@ -47,7 +47,7 @@ export default function Journal() {
         {/* Title */}
         <div data-aos="fade-up" className="mb-10">
           <h1 className="text-3xl md:text-4xl font-bold text-gray-900 mb-4">
-            The Journal of the Society
+            The Journal of TPAS
           </h1>
           <p className="text-lg text-gray-700 ">
             Welcome to TPAS our official Journal — a curated platform for
