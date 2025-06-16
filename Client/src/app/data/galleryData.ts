0f503e4edef67b14492bb0669c9ee1b76a396880
Md. Nasir Uddin <bsse1310@iit.du.ac.bd>
2025-06-16 18:43:51 +0600

    cover changed event 1

--- Client/src/app/data/galleryData.ts
+++ Client/src/app/data/galleryData.ts
@@ -1,12 +1,4 @@
 const galleryData = [
-  {
-    slug: "cpd_and_networking_event_june_2025",
-    title: "CPD and Networking Event, June 2025",
-    description: "A day of CPD and networking for TPAS members.",
-    date: "June, 2025",
-    imageUrl: "/gallery/cpd_and_networking_event_june_2025/cover.jpg",
-    imageCount:22,
-  },
     {
     slug: "meet_the_press",
     title: "Meet the press, 04 April 2025",
@@ -14,6 +6,14 @@ const galleryData = [
     date: "04 April, 2025",
     imageUrl: "/gallery/meet_the_press/cover.jpg",
     imageCount:23,
+  },
+  {
+    slug: "cpd_and_networking_event_june_2025",
+    title: "CPD and Networking Event, June 2025",
+    description: "A day of CPD and networking for TPAS members.",
+    date: "June, 2025",
+    imageUrl: "/gallery/cpd_and_networking_event_june_2025/cover.jpg",
+    imageCount:22,
   },
 
   // Add more gallery items here
