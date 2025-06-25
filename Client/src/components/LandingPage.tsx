e192c9b54c268d7135e3397147d1c59c196665de
Nasir-1310 <bsse1310@iit.du.ac.bd>
2025-06-26 02:05:03 +0600

    font size changed landin page

--- Client/src/components/LandingPage.tsx
+++ Client/src/components/LandingPage.tsx
@@ -48,7 +48,7 @@ export default function LandingPage() {
 
         {/* Hero Content */}
         <div className="absolute inset-0 z-40 flex flex-col items-center justify-center text-center text-white px-4 md:px-8">
-          <h1 className="text-2xl  md:text-4xl xl:text-5xl font-bold mb-4 leading-tight max-w-5xl">
+          <h1 className="text-xl sm:text-3xl  md:text-4xl xl:text-5xl font-bold mb-4 leading-tight max-w-5xl">
             The Professional Accountants&apos; Society
           </h1>
 
@@ -56,20 +56,20 @@ export default function LandingPage() {
             A UK-based organisation representing British Bangladeshi Chartered and Chartered Certified Accountants.
           </p>
 
-          <p className="text-sm md:text-base text-white max-w-3xl mb-5 md:mb-8 leading-relaxed hidden md:block">
+          <p className="text-xs sm:text-lg text-white max-w-3xl mb-4 leading-relaxed">
             We create an environment where members share knowledge, skills, and experience — empowering individuals while
             contributing positively to the UK’s wider business and professional community.
           </p>
 
           {/* Buttons */}
-          <div className="flex flex-col md:flex-row items-center gap-2 md:gap-4">
-            <Rounded_Button href="/members/membership-and-fees" className="bg-transparent border-white text-white hover:text-[#0b0b5b]">
+          <div className="flex flex-col md:flex-row items-center gap-2 md:gap-4 ">
+            <Rounded_Button href="/members/membership-and-fees" className=" text-xs sm:text-lg bg-transparent border-white text-white hover:text-[#0b0b5b]">
               Membership & Fees
             </Rounded_Button>
-            <Rounded_Button href="/members/benefits" className="bg-transparent border-white text-white hover:text-[#0b0b5b]">
+            <Rounded_Button href="/members/benefits" className="text-xs sm:text-lg bg-transparent border-white text-white hover:text-[#0b0b5b]">
               Member Benefits
             </Rounded_Button>
-            <Rounded_Button href="/contact-us" className="bg-transparent border-white text-white hover:text-black">
+            <Rounded_Button href="/contact-us" className="text-xs sm:text-lg bg-transparent border-white text-white hover:text-black">
               Join Us
             </Rounded_Button>
           </div>
