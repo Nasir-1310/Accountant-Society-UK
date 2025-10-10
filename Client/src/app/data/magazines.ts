9c4b8eb50b4dd662759c7423abf47cd84ba76bee
Md. Nasir Uddin <bsse1310@iit.du.ac.bd>
2025-10-10 11:55:48 +0600

    magazine description updated

--- Client/src/app/data/magazines.ts
+++ Client/src/app/data/magazines.ts
@@ -7,15 +7,17 @@ type Magazine = {
   description: string;
 };
 
+
+
 const magazines: Magazine[] = [
   {
-    slug: "accountants-day-2025",
+    slug: "TPAS-Magazine-Accountant's-day-2025",
     title: "TPAS Magazine - Accountant's day-2025",
     cover: "/magazine/images/magazine1_cover.png",
     pdf: "https://drive.google.com/file/d/1X0ZpbAtMGVNKEOlQvZ3CJGB-wxqsbkZW/view?usp=sharing",
     date: "2025-10-07",
     description:
-      "The January 2025 edition covers key insights into sustainable accounting practices, leadership stories, and community highlights.",
+      "This year magazine covers Sustainability, Personal Finance, Halal Mortgage and British Bangladeshi Accountants’ contributions to the UK and Bangladesh Economy.",
   },
 ];
 
